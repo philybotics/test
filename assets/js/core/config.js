733cd76cc4affe711731423713ed41b66e0eb26b
PHILYBOTICS™ <philybotics@gmail.com>
2026-05-02 03:52:27 +0000

    Add configuration file for application settings

---
+++ assets/js/core/config.js
@@ -0,0 +1,13 @@
+export const CONFIG = {
+    APP_NAME: "MultiPlatform Hub",
+    ADMIN_PASSWORD: "1234",           // Change this later for security
+    DEFAULT_THEME: "theme-blue",
+    STORAGE_KEYS: {
+        THEME: "mp_theme",
+        SIDEBARS: "mp_sidebars",
+        BLOG_POSTS: "mp_blog_posts",
+        PRODUCTS: "mp_products",
+        APPS: "mp_apps",
+        SETTINGS: "mp_settings"
+    }
+};
