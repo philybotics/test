2938db005ac79bcd3555f706a79f374a146d7643
PHILYBOTICS™ <philybotics@gmail.com>
2026-05-02 04:39:21 +0000

    Change exported functions to local functions

--- assets/js/core/utils.js
+++ assets/js/core/utils.js
@@ -1,12 +1,12 @@
-export function $(selector) {
+function $(selector) {
     return document.querySelector(selector);
 }
 
-export function \[ (selector) {
+function $$(selector) {
     return document.querySelectorAll(selector);
 }
 
-export function createElement(tag, className = '', innerHTML = '') {
+function createElement(tag, className = '', innerHTML = '') {
     const el = document.createElement(tag);
     if (className) el.className = className;
     if (innerHTML) el.innerHTML = innerHTML;
